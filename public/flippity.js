e1fa9ba4f84b98b8ba8664ea90e310b850f90e31
2Peti <44583946+2Peti@users.noreply.github.com>
2022-06-03 15:14:19 +0200

    Add files via upload

---
+++ public/flippity.js
@@ -0,0 +1,22 @@
+for (let nm = 0; nm < data.length; nm++) {
+  document.getElementById('al' + nm).style.display = "none"
+  document.getElementById('ul' + nm).style.display = "inline-block"
+  document.getElementById('a' + nm).disabled = true
+  document.getElementById('k' + nm).style.display = "none"
+  corrects[nm] = true
+  givens[nm] = data[nm][2]
+  if (seq == "yes") {
+    document.getElementById('dl' + nm).style.display = "none"
+    document.getElementById('al' + nm).style.display = "inline-block"
+  }
+  else if (seq == "final") {
+    document.getElementById('dl' + penUlt).style.display = "none"
+    document.getElementById('al' + penUlt).style.display = "inline-block"
+  }
+} 
+
+document.getElementById('time').innerHTML = formatTime()
+document.getElementById('doneScreen').style.display = "block"
+if (email != "") {
+  sendResults()
+}
